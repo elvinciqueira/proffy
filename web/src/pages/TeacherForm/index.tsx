40ee97bc064b193d8d9d19d8d938de6af2156161
elvinciqueira <elvinciqueira@gmail.com>
2020-08-05 21:27:28 -0300

    feat: estruturando campo de horário

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
+import TextArea from "../../components/Textarea";
+import Select from "../../components/Select";
 
 import warningIcon from "../../assets/images/icons/warning.svg";
 
 import "./styles.css";
 
 const TeacherForm: React.FC = () => {
+  const [scheduleItems, setScheduleItems] = useState([
+    {
+      week_day: 0,
+      from: "",
+      to: "",
+    },
+  ]);
+
+  const addNewScheduleItem = useCallback(() => {
+    setScheduleItems((scheduleItem) => [
+      ...scheduleItem,
+      {
+        week_day: 0,
+        from: "",
+        to: "",
+      },
+    ]);
+
+    console.log("não funciona");
+  }, []);
+
   return (
     <div id="page-teacher-form" className="container">
       <PageHeader
@@ -22,12 +45,62 @@ const TeacherForm: React.FC = () => {
           <Input name="name" type="text" label="Nome Completo" />
           <Input name="avatar" type="text" label="Avatar" />
           <Input name="whatsapp" type="text" label="Whatsapp" />
+          <TextArea name="bio" label="Biografia" />
         </fieldset>
         <fieldset>
           <legend>Sobre à aula</legend>
 
-          <Input name="subject" type="text" label="Matéria" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Física", label: "Física" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Química", label: "Química" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "Filosofia", label: "Filosofia" },
+              { value: "História", label: "História" },
+              { value: "Português", label: "Português" },
+              { value: "Educação Física", label: "Educação Física" },
+            ]}
+          />
           <Input name="cost" type="text" label="Custo por sua hora por aula" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis{" "}
+            <button type="button" onClick={addNewScheduleItem}>
+              + Novo horário
+            </button>
+          </legend>
+
+          {scheduleItems.map((scheduleItem, index) => (
+            <div key={index} className="schedule-item">
+              <Select
+                name="subject"
+                label="Matéria"
+                options={[
+                  { value: "Artes", label: "Artes" },
+                  { value: "Biologia", label: "Biologia" },
+                  { value: "Ciências", label: "Ciências" },
+                  { value: "Física", label: "Física" },
+                  { value: "Matemática", label: "Matemática" },
+                  { value: "Química", label: "Química" },
+                  { value: "Geografia", label: "Geografia" },
+                  { value: "Filosofia", label: "Filosofia" },
+                  { value: "História", label: "História" },
+                  { value: "Português", label: "Português" },
+                  { value: "Educação Física", label: "Educação Física" },
+                ]}
+              />
+              <Input name="from" label="Das" type="time" />
+              <Input name="to" label="Até" type="time" />
+            </div>
+          ))}
         </fieldset>
 
         <footer>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
 import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 import "./styles.css";
 
@@ -11,8 +12,37 @@ const TeacherList: React.FC = () => {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <Input type="text" name="subject" label="Matéria" />
-          <Input type="text" name="week_day" label="Dia da semana" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Física", label: "Física" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Química", label: "Química" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "Filosofia", label: "Filosofia" },
+              { value: "História", label: "História" },
+              { value: "Português", label: "Português" },
+              { value: "Educação Física", label: "Educação Física" },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-feira" },
+              { value: "2", label: "Terça-feira" },
+              { value: "3", label: "Quarta-feira" },
+              { value: "4", label: "Quinta-feira" },
+              { value: "5", label: "Sexta-feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
+
           <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
